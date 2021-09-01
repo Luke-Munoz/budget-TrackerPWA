21b4de5cb07dab6ee8e4634b15439f5424604773
Luke <munozluke1@gmail.com>
2021-09-01 12:33:32 -0600

    adding my wokr

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,5 @@
 let db;
-
+const indexedDB = window.indexedDB;
 const request = indexedDB.open('budgetDB', 1);
 
 request.onupgradeneeded = function(event) {
@@ -8,8 +8,57 @@ request.onupgradeneeded = function(event) {
 }
 request.onsuccess = function(event) {
     db = event.target.result;
+    if (navigator.online) {
+        uploadBudget()
+    }
+};
 
-    // if(navigator.online) {
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
 
-    // }
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget', 'readwrite']);
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    budgetObjectStore.add(record);
 }
+
+function uploadBudget() {
+    const transaction = db.transaction(['new_budget', 'readwrite']);
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    const getAll = budgetObjectStore.getAll();
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk'), {
+                    method: 'POST',
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    }
+
+                }
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_budget', 'readwrite']);
+                    const budgetObjectStore = transaction.objectStore('new_budget')
+
+                    budgetObjectStore.clear()
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }
+
+}
+
+
+window.addEventListener('online', uploadBudget);
